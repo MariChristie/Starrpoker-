7ccd8fe7f23a410e9f9a71b762b632984f78d989
= <cavaleiromiau@gmail.com>
2025-05-11 23:54:05 -0300

    Add button and flex background
    Basic Menu

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ window = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("StarrPoker")
 
 background = pygame.image.load("imagens/background.png").convert()
+bg_rect = background.get_rect(center=(800 // 2, 600 // 2))
 
-start_button = Button("imagens/start_button.png")
+start_button = Button("imagens/start_button.png", (300, 150), 0.65)
+exit_button = Button("imagens/exit_button.png", (300, 280), 0.65)
+
 
 clock = pygame.time.Clock()
 
@@ -18,9 +21,18 @@ while True:
             pygame.quit()
             sys.exit()
 
+    if start_button.is_pressed():
+        print("Start Button pressed")
+
+    if exit_button.is_pressed():
+        pygame.quit()
+        sys.exit()
+
     window.fill("black")
-    window.blit(background, (0, 0))
+    window.blit(background, bg_rect)
     start_button.draw(window)
+    exit_button.draw(window)
 
     pygame.display.flip()
     clock.tick(60)
+
